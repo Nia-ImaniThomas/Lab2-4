ae9f93b2b5f924d59d84bd4abc8dc7bd9068dd45
Nia-Imani Thomas <61117975+Nia-ImaniThomas@users.noreply.github.com>
2020-02-16 12:46:20 -0600

    Add files via upload

---
+++ Lab 2-4_Nia-Imani Thomas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lab_2_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome To Lab 2-4_Nia-Imani Thomas");
+            Console.WriteLine("Working with HTML");
+        }
+    }
+}
